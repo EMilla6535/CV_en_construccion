80fb8bb57a049483b2ab4d6f5cc767b0737ee4e5
Emiliano Milla <milla_e_6535@yahoo.com>
2023-01-19 23:23:34 -0300

    Nuevo boton, mejoras en los estilos y comentarios

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+/* Datos por defecto si no hay Internet */
 let default_user_data = {"results":[{
 	"gender":"male",
 	"name":{
@@ -13,8 +14,8 @@ let default_user_data = {"results":[{
 		"country":"Australia",
 		"postcode":2089,
 		"coordinates":{
-			"latitude":"-85.9637",
-			"longitude":"32.7085"},
+			"latitude":"-30.296453",
+			"longitude":"153.114022"},
 		"timezone":{
 			"offset":"+2:00",
 			"description":"Kaliningrad, South Africa"}},
@@ -29,7 +30,7 @@ let default_user_data = {"results":[{
 		"medium":"img/1-med.jpg",
 		"thumbnail":"img/1-thumbnail.jpg"},
 	"nat":"AU"}]};
-
+/* Traduce Nacionalidad */
 function getNationality(nat){
 	if(nat == "AU" || nat == "au"){
 		return "Australiano";
@@ -42,6 +43,7 @@ function getNationality(nat){
 	}
 	return "Sin Nacionalidad";
 }
+/* Traduce sexo */
 function getGender(gender){
 	if(gender == "male"){
 		return "Masculino";
@@ -51,12 +53,14 @@ function getGender(gender){
 	}
 	return "No Indicado";
 }
+/* Traduce pais */
 function getCountry(country){
 	if(country == "Spain"){
 		return "España";
 	}
 	return country;
 }
+/* Obtiene el año como string */
 function getYear(date){
 	let year = "";
 	for(let i = 0; i < 4; i++){
@@ -64,6 +68,7 @@ function getYear(date){
 	}
 	return year;
 }
+/* Obtiene el mes como string */
 function getMonth(date){
 	let month = "";
 	for(let i = 5; i < 7; i++){
@@ -71,6 +76,7 @@ function getMonth(date){
 	}
 	return month;
 }
+/* Obtiene el dia como string */
 function getDay(date){
 	let day = "";
 	for(let i = 8; i < 10; i++){
@@ -78,11 +84,13 @@ function getDay(date){
 	}
 	return day;
 }
+/* Obtiene la fecha como DD/MM/AAAA en string */
 function getDate(date){
 	let buffer = "";
 	buffer = getDay(date) + "/" + getMonth(date) + "/" + getYear(date);
 	return buffer;
 }
+/* Establece los datos de usuario en los diferentes campos */
 function setUserData(user_data){
 	document.getElementById('nombre').innerHTML = user_data.results[0].name.first;
 	document.getElementById('apellido').innerHTML = user_data.results[0].name.last;
@@ -93,8 +101,6 @@ function setUserData(user_data){
 	document.getElementById('perfil').innerHTML = '<img class="img-perfil" alt="Imagen de Perfil" src="' + user_data.results[0].picture.large + '">'
 
 	let user_location = user_data.results[0].location;
-	let latitud = user_location.coordinates.latitude;
-	let longitud = user_location.coordinates.longitude;
 
 	document.getElementById('calle').innerHTML = user_location.street.number + " " + user_location.street.name;
 	document.getElementById('ciudad').innerHTML = user_location.city;
@@ -102,13 +108,34 @@ function setUserData(user_data){
 	document.getElementById('pais').innerHTML = getCountry(user_location.country);
 	document.getElementById('cp').innerHTML = user_location.postcode;
 	
+	/* Si no hay Internet, mostrar un mensaje en lugar de un mapa */
 	if(!navigator.onLine){
-		document.getElementById('map').innerHTML = '<p class="domicilio-map-error">Error al cargar el mapa. Revise su conexión a Internet</p>';
+		document.getElementById('map').innerHTML = '<p class="domicilio-no-map">Error al cargar el mapa. Revise su conexión a Internet</p>';
 	}
 	else{
+		/* Coordenadas predefinidas porque las que vienen con los datos no se visualizan correctamente */
+		let latitud = "";
+		let longitud = "";
+		if(getNationality(user_data.results[0].nat) == "Australiano"){
+			latitud = "-33.850502";
+			longitud = "151.008227";
+		}
+		else if(getNationality(user_data.results[0].nat) == "Español"){
+			latitud = "40.193919";
+			longitud = "-3.254973";
+		}
+		else if(getNationality(user_data.results[0].nat) == "Mexicano"){
+			latitud = "22.424028";
+			longitud = "-102.238091";
+		}
+		else{
+			latitud = "-30.296453";
+			longitud = "153.114022";
+		}
+		/* Añade el mapa y un marcador */
 		let map = L.map('map').setView([latitud, longitud], 13);
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		    maxZoom: 19,
+		    maxZoom: 13,
 		    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 		}).addTo(map);
 		let marker = L.marker([latitud, longitud]).addTo(map);
@@ -119,6 +146,7 @@ function setUserData(user_data){
 	document.getElementById('celular').innerHTML = user_data.results[0].cell;
 }
 
+/* Trata de obtener los datos de usuario */
 fetch("https://randomuser.me/api/?exc=login,registered,id&noinfo&format=json&gender=male&nat=au,mx,es")
   .then(function (response) {
     return response.json();
@@ -129,3 +157,11 @@ fetch("https://randomuser.me/api/?exc=login,registered,id&noinfo&format=json&gen
   .catch(function(error) {
 	  setUserData(default_user_data);
   })
+
+/* Al hacer click en un boton mostrar un mapa */
+function abrirMapa(){
+	  document.getElementById("boton-mapa").style.display = "none";
+	  document.getElementById("map").style.display = "block";
+}
+document.getElementById("boton-mapa").addEventListener("click", abrirMapa, false);
+
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,5 @@
-
+/* Slideshow(carrusel) de imagenes mostrando una resaltada en el centro y dos semi-transparente
+ * a los lados. */
 let prev_slide_index = 0;
 let slide_index = 1;
 let next_slide_index = 2;
@@ -6,24 +7,31 @@ let next_slide_index = 2;
 let stand_by = setTimeout(funcAnimar, 1000);
 let animacion;
 
+/* Verifica que el indice de image no se vaya de los limites */
 function checkIndex(index, length){
 	if((index + 1) >= length){
 		return 0;
 	}
 	return ++index;
 }
-
+/* Funcion que comienza la animacion */
 function funcAnimar(){
+	/* Obtiente todas las imagenes */
 	let slides = document.getElementsByClassName('slide');
-	
+	/* Les remueve los id para cambiar los estilos */
 	slides[prev_slide_index].removeAttribute('id');
 	slides[slide_index].removeAttribute('id');
 	slides[next_slide_index].removeAttribute('id');
-	
+	/* Establece las clases para mover a la izquierda */
 	slides[prev_slide_index].setAttribute('class', 'slide slide-left left-out col-3');
 	slides[slide_index].setAttribute('class', ' slide slide-left left-move1 col-3');
 	slides[next_slide_index].setAttribute('class', 'slide slide-left left-move2 col-3');
-	
+	/* Casos especiales cuando tiene que volver a empezar.
+	 * La antepenúltima, penúltima y última imagen tiene que tener
+	 * clases especiales con efectos de animación especial para
+	 * que se vean bien. Si no se hace así, entonces cuando vuelva
+	 * a empezar por la primera imagen, las posiciones relativas
+	 * hacen que se visualizen mal.*/
 	let next_index = checkIndex(next_slide_index, slides.length);
 	switch(next_index)
 	{
@@ -42,7 +50,7 @@ function funcAnimar(){
 		default:
 			break;
 	}
-	
+	/* Recalcula los indices de la 1°(semi-transparente), 2°(resaltada), 3°(semi-transparente) imagen */
 	if((slide_index + 1) >= slides.length){
 		slide_index = 0;
 		prev_slide_index++;
@@ -53,7 +61,10 @@ function funcAnimar(){
 		prev_slide_index = checkIndex(prev_slide_index, slides.length);
 		next_slide_index = checkIndex(next_slide_index, slides.length);
 	}
-	
+	/* Casos especiales cuando tiene que volver a empezar después de
+	 * recalcular los indices. Es similar al caso anterior, pero ésta
+	 * vez es con las primeras tres imágenes que deben tener clases y
+	 * efectos de animación especiales para una correcta visualización. */
 	switch(next_slide_index)
 	{
 		case 0:
@@ -72,33 +83,38 @@ function funcAnimar(){
 			slides[next_slide_index].setAttribute('class', 'slide slide-left left-in col-3');
 			break;
 	}
-	
+	/* Hace que los contenedores de imágenes no estén ocultos */
 	slides[slide_index].setAttribute('id', 'actual');
 	slides[slide_index].style.display = 'block';
 	slides[prev_slide_index].setAttribute('id', 'side');
 	slides[prev_slide_index].style.display = 'block';
 	slides[next_slide_index].setAttribute('id', 'side');
 	slides[next_slide_index].style.display = 'block';
-	
+	/* Reinicia el proceso de animación y llama a una función
+	 * cuando se termina de animar. */
 	clearTimeout(stand_by);
 	animacion = setTimeout(funcStand, 1000);
 }
-
+/* Función que se llama cuando se detiene la animación */
 function funcStand(){
+	/* Obtiene todas las imágenes */
 	let slides = document.getElementsByClassName('slide');
+	/* Para todas las imágenes que no tienen id, ocultarlas */
 	let i;
-	
 	for(i = 0; i < slides.length; i++){
 		if(!slides[i].hasAttribute('id')){
 			slides[i].style.display = 'none';
 			slides[i].style.left = '0%';
 		}
 	}
-	
+	/* Establece clases iguales para las imágenes */
 	slides[prev_slide_index].setAttribute('class', 'slide col-3');
 	slides[slide_index].setAttribute('class', 'slide  col-3');
 	slides[next_slide_index].setAttribute('class', 'slide col-3');
 	
+	/* Casos especiales cuando la última imágen visualizada es la
+	 * primera, segunda ó tercera. Se mueve su posición relativa
+	 * para una correcta visualización. */
 	switch(next_slide_index)
 	{
 		case 0:
@@ -119,7 +135,8 @@ function funcStand(){
 		default:
 			break;
 	}
-	
+	/* Reinicia el proceso de animación y llama a una función
+	 * para comenzar a animar. */
 	clearTimeout(animacion);
 	stand_by = setTimeout(funcAnimar, 1000);
 }
